cd8d2ca976d767591ce21a2d1add126b3dfaa548
ll9 <llwierer@web.de>
2018-10-23 22:07:59 +0200

    basic change of cursor

--- ProgressCursor/Form1.cs
+++ ProgressCursor/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,13 @@ namespace ProgressCursor
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.AppStarting;
+            await Task.Run(() => Thread.Sleep(3000));
+            Cursor = Cursors.Default;
         }
     }
 }
